791821753557b89f9389fd713469b148b4e446ed
Nitin Marale <66687755+nitinmarale@users.noreply.github.com>
2020-09-06 13:29:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-var text = ["I like Coffee ☕", "I like Trading 💙", "I like  Travelling 🚘"];
-var counter = 0;
-var elem = document.getElementById("changeText");
-var inst = setInterval(slideit, 2000);
-
-function slideit() {
-  elem.innerHTML = text[counter];
-  counter++;
-  if (counter >= text.length) {
-    counter = 0;
-    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
-  }
-}
 
 var submit =document.getElementById('btn-submit');
 submit.addEventListener('click', function(){
     alert("Sorry !!!Form is not working ");
 });
+
+
+
+// var backGround = document.getElementById('bg-color');
+// var bgColor = document.getElementById('dark-mode');
+
+
+// backGround.addEventListener('click',function(){
+//   bgColor.classList.toggle('dark-mode')
+
+// })
+
+function makeBold(){
+  document.getElementById('dark-mode').classList.toggle('dark')
+  
+  
+}
+
+
+   
+    
+
+
+  
+
+    
